4e62d6ab843810c16890d82fd63c2dd1e7bfdeb4
Scott Stanfield <scott@vertigo.com>
2015-04-06 14:06:45 -0700

    updated to port 8888

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var port = 5000;
+var port = 8888;
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('hello from node\n');
