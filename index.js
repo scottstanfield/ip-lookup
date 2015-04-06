ff5c98e071e5b5d8b82ce8a7d01e06ada9dd7b1c
Scott Stanfield <scott@vertigo.com>
2015-04-06 13:32:03 -0700

    removed unused files and simplified

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var http = require('http');
 var port = Number(process.env.PORT || 3002);
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    console.log(req.headers['x-forwarded-for']);
-    res.end(req.headers['x-forwarded-for'] + "\n");
-    // Client address in request -----^
+    res.end('hello from node\n');
 });
 server.listen(port);
