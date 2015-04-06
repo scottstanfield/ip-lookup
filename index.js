249719bc15489d0f627772fd70242fcc70f37042
Scott Stanfield <scott@vertigo.com>
2015-04-06 15:14:23 -0700

    updated messages

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 var port = 8888;
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('hello from node\n');
+    res.end('hello from node version 2\n');
 });
 server.listen(port);
