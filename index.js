17cfc65073fc0dee05c189ad8f81065ecc1a3a7e
Scott Stanfield <scott@vertigo.com>
2015-04-06 15:37:21 -0700

    another random change

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 var port = 8888;
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('hello from node version 2\n');
+    res.end('hello from node version 3 and docker and systemctl\n');
 });
 server.listen(port);
