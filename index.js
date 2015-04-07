2a5fe09e7feab0d69b3276521ef69872034fe0ca
Scott Stanfield <scott@vertigo.com>
2015-04-07 14:27:03 -0700

    changed text for pinch

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 var port = 8888;
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('hello from node version 3 and docker and systemctl\n');
+    res.end('code review for pinch\n');
 });
 server.listen(port);
